55c261ec7731253488c7a37c095cb4cd78c522c2
Grayson Smith <graysonsmith91@gmail.com>
2022-12-12 10:55:05 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// You fill in some sample weekly miles traveled in this array
+const weeklyMiles = [22.5, 2.9, 35, 0, 12.3];
+
+// Declare a variable to store the total. Initial value is 0.
+let totalMiles = 0
+
+/*
+	Iterate the array of miles with a for..of loop.
+	Add each mileage to the total mileage variable.
+*/
+for (const dailyMiles of weeklyMiles) {
+    totalMiles += dailyMiles
+}
+
+// Declare a new variable to store the average miles over time
+const averageMiles = totalMiles / weeklyMiles.length
+
+
+// Output the average miles and the total miles to the console
+console.log(`I average ${averageMiles} miles each week. I have driven a total of ${totalMiles} miles.`)
